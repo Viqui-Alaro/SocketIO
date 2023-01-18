f841eef6503224957e80863f12f1ccc5be2d6608
viqui alaro <viqui.alaro@impuestos.gob.bo>
2023-01-18 16:00:54 -0400

    Implementacion de ejemplos con ON, Once y off

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ const express = require("express");
 const path = require("path");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
+const { Socket } = require("dgram");
 
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer);
 
 app.use(express.static(path.join(__dirname,"views")))
+
+const socketsOnline =[];
 
 app.get("/", (req,res)=>{
     res.sendFile(__dirname + "/views/index.html");
@@ -16,9 +19,11 @@ app.get("/", (req,res)=>{
 
 io.on("connection", socket =>{
 
+    socketsOnline.push(socket.id);
+
     // Emisión Básica
     socket.emit("Welcome","Ahora estás conectado.");
-    
+
     socket.on("server",data=>{
         console.log(data);
     });
@@ -26,6 +31,31 @@ io.on("connection", socket =>{
     // Emisión a Todos
     io.emit("everyone", socket.id + " se ha conectado ");
 
+
+     // Emisión a uno solo
+     socket.on("last", message=>{
+
+       const lastSocket= socketsOnline[socketsOnline.length(-1)];
+       //const lastSocket= socketsOnline.at(-1); 
+       io.to(lastSocket).emit("salute",message);
+
+     });
+
+/*
+     socket.emit("on", "holi");
+     socket.emit("on", "holi");
+
+         socket.emit("once","holi");
+     socket.emit("once","holi");
+
+*/
+
+
+     socket.emit("off","holi");
+     setTimeout(()=>{
+        socket.emit("off","holi");
+     },3000);
+ 
 });
 
 
--- src/views/js/index.js
+++ src/views/js/index.js
@@ -19,3 +19,32 @@ emitToServer.addEventListener("click",()=>{
 socket.on("everyone",message =>{
     console.log(message);
 });
+
+const emitToLast = document.querySelector("#emit-to-last");
+emitToServer.addEventListener("click",()=>{
+    socket.emit("last","Hola");
+});
+
+socket.on("salute",message =>{
+    console.log(message);
+});
+
+socket.on("on",() =>{
+    console.log("Se emite varias veces");
+});
+
+
+socket.once("once",() =>{
+    console.log("Se emite una sola vez");
+});
+
+
+const listener =()=>{
+    console.log("Se apaga el evento");
+}
+
+socket.on("off",listener);
+setTimeout(()=>{
+
+    socket.off("off",listener);
+},2000);
